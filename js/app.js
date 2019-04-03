b0db54b9691817f57b921e6c32c77267d3625e4e
Justin Atkins <justinatkins@Justins-MacBook-Pro.local>
2019-04-03 10:04:51 -0500

    submitting for deploy

---
+++ js/app.js
@@ -0,0 +1,59 @@
+console.log('Welcome to Tomagotchi Town')
+
+
+class pet {
+	constructor(name){
+		this.name = name;
+		this.hungry = 0;
+		this.sleepy = 0;
+		this.boredom = 0;
+		this.age = 0
+
+// function startGame() {
+// 	myGamePiece = new component (30, 30, "http://archive.boston.com/ae/movies/blog/ignignokt.gif", 10, 120, "ignignokt");
+
+}
+
+
+const game = {
+  	time: 0,
+  	start(){
+
+
+		const gameTimer = setInterval(() => {
+			if(this.time === 60){
+        	console.log('Your pet has died....');
+	       	clearInterval(gameTimer);
+        	}
+
+        	this.time += 1;
+
+        	console.log(this.time);
+    	}, 1000);
+    }
+}
+
+game.start();
+
+$('form').on('go', (e) => {
+    game.startGame();
+    event.preventDefault();
+    
+}
+
+$('#hungry').on('click', () => {
+    game.hungry();
+
+})
+
+$('#bored').on('click', () => {
+    game.bored();
+
+})
+
+$('#sleepy').on('click', () => {
+    game.sleepy();
+
+});
+
+
